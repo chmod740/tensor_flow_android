ff2d5038412c5a5048555e621d5e03b7253b3cfb
hupeng <hupeng@imudges.com>
2016-12-13 16:25:06 +0800

    add try except block

--- socket_for_android.py
+++ socket_for_android.py
@@ -1,10 +1,7 @@
 import socketserver
-import struct
 from socketserver import StreamRequestHandler as SRH
-from time import ctime
 from fft_test import fft
 from tensorflow_test import *
-
 from numpy import asarray
 
 #socket IP 端口
@@ -19,20 +16,21 @@ class Servers(SRH):
         print(self.client_address)
         # self.wfile.write('connection %s:%s at %s succeed!' % (host,port,ctime()))
         while True:
-            data = self.request.recv(1280)
-
-            test = data.decode('utf-8')
-            test = test.replace("\n", "")
-            fft_final = fft(test)#fft函数调用
-            data = tf_mul(fft_final)#tensorflow矩阵乘法调用
-            # print(asarray(data))
-            # print(type(asarray(data).tolist()))
-            # print(len(asarray(data).tolist()))
-            for element in asarray(data).tolist():
-                for element1 in element:
-                    print(element1)
-            self.request.send(data)
-
+            try:
+                data = self.request.recv(1280)
+                test = data.decode('utf-8')
+                test = test.replace("\n", "")
+                fft_final = fft(test)#fft函数调用
+                data = tf_mul(fft_final)#tensorflow矩阵乘法调用
+                # print(asarray(data))
+                # print(type(asarray(data).tolist()))
+                # print(len(asarray(data).tolist()))
+                for element in asarray(data).tolist():
+                    for element1 in element:
+                        print(element1)
+                self.request.send(data)
+            except:
+                pass
 #开启socketservice
 print("server is running....")
 server = socketserver.ThreadingTCPServer(addr,Servers)
